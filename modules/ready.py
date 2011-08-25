2eecf244c219ab9369fc9c49268db50ef20e12b3
Sebastian Kreutzberger <s.kreutzberger@googlemail.com>
2011-08-25 13:00:16 +0200

    delete unnecessary file

--- modules/ready.py
+++
@@ -1,7 +0,0 @@
-# module to interact with readability
-#import readability
-
-#token = readability.xauth('consumer-key', 'consumer-secret', 'username', 'password')
-#rdd = readability.oauth('consumer-key', 'consumer-secret', token=token)
-
-# 
